dff0c2c1cab2afc3d8b15e6f7ab4e88fd64ba833
Alex Martinez Senent <alex.martinez01250@gmail.com>
2021-11-21 12:59:01 +0100

    Added spray plot

--- test/data_acq.py
+++ test/data_acq.py
@@ -10,6 +10,7 @@ class Data_visualizer(Observer):
         self.interval = interval
         Observer.__init__(self)
         self.spray_quantity = 0
+        self.spray_data = np.array([10])
         self.observe('spray', self.spray_callback)# Listening to events 'spray' and calling method cure if trigger
         self.observe('field_data', self.field_callback)
         self.colormap = ["#c8c8c8", "#ff0000", "#ffa500", "#00ff00"]
@@ -21,15 +22,33 @@ class Data_visualizer(Observer):
     def field_callback(self, data_list):
         np_data = np.array(data_list)/(15*15)
         self.field_data= np.vstack([self.field_data, np_data])
+        self.spray_data = np.vstack([self.spray_data, self.spray_quantity])
 
-    def plot_data(self):
-        x_axis = np.arange(0,len(self.field_data)*self.interval,self.interval)
-        plt.stackplot(x_axis, self.field_data[:,3],self.field_data[:,2],self.field_data[:,1],self.field_data[:,0],colors=self.colormap, labels=self.labels_field)
+
+    
+
+    def plot_data_field(self):
+        self.x_axis = np.arange(0,len(self.field_data)*self.interval,self.interval)
+        plt.stackplot(self.x_axis, self.field_data[:,3],self.field_data[:,2],self.field_data[:,1],self.field_data[:,0],colors=self.colormap, labels=self.labels_field)
         plt.legend(loc='upper left')
         plt.title("Field crops' health evolution")
         plt.xlabel('Hours')
         plt.ylabel('Number of crops')
         plt.savefig('field_data.png')
+
+    def plot_data_spray(self):
+        plt.clf()
+        plt.plot(self.x_axis,self.spray_data[:-1])
+        plt.title("Spray usage")
+        plt.xlabel('Hours')
+        plt.ylabel('Spray actions')
+        plt.savefig('spray_data.png')
+
+    def plot_data(self):
+        self.plot_data_field()
+        self.plot_data_spray()
+        # self.plot_data_detection()
+
     def data_plot_pipeline(self):
         
         while True:

1e79dedca46eb97304c3883738ed5958f10c20b1
Alex Martinez Senent <alex.martinez01250@gmail.com>
2021-11-22 08:35:15 +0100

    Added scanning performance plot

--- test/data_acq.py
+++ test/data_acq.py
@@ -8,24 +8,34 @@ class Data_visualizer(Observer):
     def __init__(self, interval):
         self.field_data = np.array([[100,0,0,0]])
         self.interval = interval
+        self.detections = np.array([0,0])
+        self.detections_data = np.array([0])
+        self.detections_cnt = 0
         Observer.__init__(self)
         self.spray_quantity = 0
         self.spray_data = np.array([10])
         self.observe('spray', self.spray_callback)# Listening to events 'spray' and calling method cure if trigger
         self.observe('field_data', self.field_callback)
+        self.observe('sick_plant', self.detection_callback)
         self.colormap = ["#c8c8c8", "#ff0000", "#ffa500", "#00ff00"]
         self.labels_field = ["Dead ", "Critical", "Infected", "Healthy"]
         
     def spray_callback(self):
         self.spray_quantity += 1
 
+    def detection_callback(self, pair):
+        if self.detections_cnt is 0:
+            self.detections = np.array(pair)
+            self.detections_cnt += 1
+        elif not (pair==self.detections).all().any():
+            self.detections = np.vstack([self.detections, pair])
+            self.detections_cnt += 1
+
     def field_callback(self, data_list):
         np_data = np.array(data_list)/(15*15)
         self.field_data= np.vstack([self.field_data, np_data])
         self.spray_data = np.vstack([self.spray_data, self.spray_quantity])
-
-
-    
+        self.detections_data = np.vstack([self.detections_data, self.detections_cnt])
 
     def plot_data_field(self):
         self.x_axis = np.arange(0,len(self.field_data)*self.interval,self.interval)
@@ -33,22 +43,36 @@ class Data_visualizer(Observer):
         plt.legend(loc='upper left')
         plt.title("Field crops' health evolution")
         plt.xlabel('Hours')
-        plt.ylabel('Number of crops')
+        plt.ylabel('Crops [%]')
         plt.savefig('field_data.png')
 
     def plot_data_spray(self):
         plt.clf()
-        plt.plot(self.x_axis,self.spray_data[:-1])
+        if len(self.x_axis) < len(self.spray_data):
+            plt.plot(self.x_axis,self.spray_data[:-1])
+        else: plt.plot(self.x_axis,self.spray_data)
         plt.title("Spray usage")
         plt.xlabel('Hours')
         plt.ylabel('Spray actions')
         plt.savefig('spray_data.png')
 
+    def plot_data_detection(self):
+        plt.clf()
+        detections_percentage = np.divide(self.detections_data.transpose(),np.sum(self.field_data[:,1:4],axis=1)*(15*15))*100
+        if len(self.x_axis) < len(np.transpose(detections_percentage)):
+            plt.plot(self.x_axis,np.transpose(detections_percentage)[:-1])
+        else: plt.plot(self.x_axis,np.transpose(detections_percentage))
+        plt.title("Scanning performance")
+        plt.xlabel('Hours')
+        plt.ylabel('Detected crops [%]')
+        plt.savefig('detection_data.png')
+
     def plot_data(self):
+        print("Plotting...")
         self.plot_data_field()
         self.plot_data_spray()
-        # self.plot_data_detection()
-
+        self.plot_data_detection()
+        print("Plotting finished")
     def data_plot_pipeline(self):
         
         while True:

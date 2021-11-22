c392eba31d0d27ea34e03185feb1124fc1a78fcd
0oo0ROM <s212638@student.dtu.dk>
2021-11-22 23:28:29 +0100

    add tank&filling tank

--- test/chargestation.py
+++ test/chargestation.py
@@ -2,12 +2,14 @@ import threading
 import time
 from observer import Observer
 from drone import Drone
+from spraydrone import SprayingDrone
 
 
 class ChargeStation(Observer):
     def __init__(self, capacity, charging_speed):
         Observer.__init__(self)
         self.observe('charge', self.charge)
+        self.observe('charge and fill', self.charge_and_fill)
         self.active_drones = []
         self.charging_speed = charging_speed
         self.capacity = capacity
@@ -17,6 +19,13 @@ class ChargeStation(Observer):
             drone.is_charging = True
             self.capacity += 1
             self.active_drones.append(drone)
+
+    def charge_and_fill(self, sprayingdrone: SprayingDrone):
+        if len(self.active_drones) < self.capacity:
+            sprayingdrone.is_charging = True
+            sprayingdrone.is_filling = True
+            self.capacity += 1
+            self.active_drones.append(sprayingdrone)
 
     def routine(self):
         while True:
@@ -28,6 +37,19 @@ class ChargeStation(Observer):
                     self.capacity -= 1
                 else:
                     drone.battery += self.charging_speed
+
+            for sprayingdrone in self.active_drones:
+                if sprayingdrone.battery >= sprayingdrone.battery_capacity:
+                    sprayingdrone.is_charging = False
+                else:
+                    sprayingdrone.battery += self.charging_speed
+                if sprayingdrone.tank >= sprayingdrone.tank_capacity:
+                    sprayingdrone.is_filling = False
+                else:
+                    sprayingdrone.tank += 20
+                if not sprayingdrone.is_filling and not sprayingdrone.is_charging:
+                    self.active_drones.remove(sprayingdrone)
+                    self.capacity -= 1
 
     def run(self):
         t1 = threading.Thread(target=self.routine)
--- test/spraydrone.py
+++ test/spraydrone.py
@@ -1,9 +1,9 @@
 import threading
 import time
 
-from drone import Drone
-from event import Event
-from fieldgenerator import FieldGenerator
+from test.drone import Drone
+from test.event import Event
+from test.fieldgenerator import FieldGenerator
 from observer import Observer
 
 
@@ -16,6 +16,9 @@ class SprayingDrone(Observer, Drone):
         self.observe('sick_plant', self.add_sick_plant)
         self.sick_plants = [[0 for c in range(world.i)] for r in range(world.j)]
         self.sick_coordinate_list = []
+        self.tank = 200
+        self.tank_capacity = 200
+        self.is_filling = False
 
     def run(self):
         t1 = threading.Thread(target=self.drone_routine)
@@ -27,6 +30,11 @@ class SprayingDrone(Observer, Drone):
                 self.go_and_spray()
             time.sleep(0.2)
 
+    def enough_tank(self):
+        if self.tank <= 20:
+            return False
+        return True
+
     def add_sick_plant(self, coordinates):
         i, j = coordinates
         self.sick_plants[i][j] = 1
@@ -34,11 +42,30 @@ class SprayingDrone(Observer, Drone):
             print('Sick crop received: {0}, {1}'.format(str(i), str(j)))
             self.sick_coordinate_list.append(coordinates)
 
+    def charge_fill_drone(self):
+        i, j = self.base_coordinates
+        while (self.position_x != i) or (self.position_y != j):
+            if self.position_x < i:
+                self.position_x += 1
+                self.battery -= 1
+            elif self.position_x > i:
+                self.position_x -= 1
+                self.battery -= 1
+
+            if self.position_y < j:
+                self.position_y += 1
+                self.battery -= 1
+            elif self.position_y > j:
+                self.position_y -= 1
+                self.battery -= 1
+            time.sleep(0.01)
+        Event('charge and fill', self)
+
     def go_and_spray(self):
-        while len(self.sick_coordinate_list) > 0 and self.enough_charge() and not self.is_charging:
+        while len(self.sick_coordinate_list) > 0 and self.enough_charge() and self.enough_tank() and not self.is_filling and not self.is_charging:
             self.spray_crop(self.sick_coordinate_list.pop(0))
-        if not self.is_charging:
-            self.charge_drone()
+        if not self.is_charging or not self.is_filling:
+            self.charge_fill_drone()
 
     def spray_crop(self, coordinates):
         i, j = coordinates
@@ -59,5 +86,8 @@ class SprayingDrone(Observer, Drone):
             time.sleep(0.01)
         if [self.position_x, self.position_y] == coordinates:
             Event('spray', [self.position_x, self.position_y])
+            self.tank -= 10
         elif not self.enough_charge():
             print("Not enough charge - returning to base")
+        elif not self.enough_tank():
+            print("Not enough tank - returning to base")
